7b20121b9618bbf431056be4d7e113fbc1a39280
Mike Johnson <mpjohnson@ra.rockwell.com>
2018-08-11 06:39:15 -0400

    Starting Chapter 4

--- chapter2/stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ chapter2/stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -9,17 +9,31 @@ import { Stock } from '../../model/stock';
 })
 export class StockItemComponent implements OnInit {
 
-  public stock: Stock;
+  public stocks: Array<Stock>;
 
   constructor() { }
 
   ngOnInit() {
-    this.stock = new Stock('Test Stock Company', 'TSC', 85, 80);
+    this.stocks = [
+      new Stock('Test Stock Company', 'TSC', 85, 80);
+      new Stock('Secondhand Stock Company', 'SSC', 10, 20),
+      new Stock('Last Stock Company', 'LSC', 876, 765),
+        new Stock('Test Stock Company', 'TSC', 85, 80);
+    new Stock('Secondhand Stock Company', 'SSC', 10, 20),
+      new Stock('Last Stock Company', 'LSC', 876, 765),
+      new Stock('Test Stock Company', 'TSC', 85, 80);
+    new Stock('Secondhand Stock Company', 'SSC', 10, 20),
+      new Stock('Last Stock Company', 'LSC', 876, 765)
+    ];
   }
 
-  toggleFavorite(event) {
+  toggleFavorite(event,index) {
     console.log('We are toggling the favorite state for this stock', event);
-    this.stock.favorite = !this.stock.favorite;
+    this.stocks[index].favorite = !this.stocks[index].favorite;
+  }
+
+  trackStockByCode(index, stock) {
+    return stock.code;
   }
 
 }
